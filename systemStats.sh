036c34215c596f4a87ae9f4cf921de64a855b064
JoelSmalls <joelokanta46@gmail.com>
2021-10-05 17:46:16 +0100

    <init> created script to get IP Temp and display version

---
+++ systemStats.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/
+# CREATED BY: You already know who it is
+# Date Created: 5/10/2021
+# Version 1
+
+# Designed to  gather system statistics.
+
+AUTHOR="Joel"
+VERSION="1"
+RELEASED="5TH OCTOBER, 2021"
+
+# Display  help message
+
+USAGE(){
+	echo -e $1
+	echo -e "\n Usage: systemStats [-t temperature] [-i ipv4 address]"
+	echo -e "\t\t [v version]"
+	echo -e "\t\t For More information see man systemStats"
+
+
+}
+
+
+# check for arguments (error checking)
+if [ $# -lt 1 ];then
+	USAGE "Not enough arguments"
+	exit 1
+elif [ $# -gt 3 ];then
+	USAGE "Too many arguments supplied"
+	exit 1
+elif [[ ( $1 == '-h') || ( $1 == '--help')]];then
+	USAGE "Help"
+	exit 1
+fi
+
+# frequently scripts are written so that arguments can be passed in any order using 'flags'
+# With  the flags method, some of the arguments can be made mandatory or optional
+# a:b (a is mandatory , b is optional) abc is all optional
+
+while getopts tiv OPTION 
+do
+case ${OPTION}
+in
+i) IP=$(ifconfig wlan0 | grep -w inet | awk '{print$2}') 
+	echo ${IP};;
+t) TEMP=$(cat /sys/class/thermal/thermal_zone0/temp)
+	echo ${TEMP} "need to divide by thousand...";;
+v) echo -e "systemStats:\n\t Version: ${VERSION} Released: ${RELEASED} Author: ${AUTHOR}";;
+
+esac
+done
+
+#end of script
